b89b8a7c09690d9468ba216db5e254aa60afa52a
Dan Arad <dan1994@gmail.com>
2019-10-14 21:32:33 +0300

    Refactored getFrequencies test to be more correct

--- test/EncoderDecoder/Huffman/HuffmanEncoderDecoderTests.cpp
+++ test/EncoderDecoder/Huffman/HuffmanEncoderDecoderTests.cpp
@@ -4,15 +4,12 @@
 
 TEST(HuffmanEncoderDecoder, getFrequencies) {
 	std::string s = "abca;;;7!W!!";
-	std::vector<HuffmanEncoderDecoder::HuffmanNode> frequencies;
-	frequencies.reserve(256);
-	std::iota(frequencies.begin(), frequencies.end(), 0);
-	frequencies['a'].frequency = 2;
-	frequencies['b'].frequency = 1;
-	frequencies['c'].frequency = 1;
-	frequencies[';'].frequency = 3;
-	frequencies['7'].frequency = 1;
-	frequencies['!'].frequency = 3;
-	frequencies['W'].frequency = 1;
-	EXPECT_EQ(HuffmanEncoderDecoder::getFrequencies(s), frequencies) << "HuffmanEncoderDecoder::getFrequencies doesn't work";
+	auto frequencies = HuffmanEncoderDecoder::getFrequencies(s);
+	EXPECT_EQ(frequencies['a']->frequency, 2);
+	EXPECT_EQ(frequencies['b']->frequency, 1);
+	EXPECT_EQ(frequencies['c']->frequency, 1);
+	EXPECT_EQ(frequencies[';']->frequency, 3);
+	EXPECT_EQ(frequencies['7']->frequency, 1);
+	EXPECT_EQ(frequencies['!']->frequency, 3);
+	EXPECT_EQ(frequencies['W']->frequency, 1);
 }
