a80f3243b4e7ba6133644c5c61e016f64b055d7f
Dan Arad <dan1994@gmail.com>
2019-10-22 03:02:15 +0300

    Revised and extended tests

--- test/Dane/DaneHeaderTests.cpp
+++ test/Dane/DaneHeaderTests.cpp
@@ -3,22 +3,22 @@
 #include <gtest/gtest.h>
 
 TEST(DaneHeader, ToString) {
-	DaneHeader dh;
-	dh.encoding = 'a';
-	dh.options = ('b' << 8) | 'c';
-	dh.checksum = ('d' << 8) | 'e';
 	std::string s;
 	s += static_cast<char>(218);
 	s += static_cast<char>(190);
 	s += '\0';
 	s += "abcde";
+	DaneHeader dh;
+	dh.encoding = 'a';
+	dh.options = ('b' << 8) | 'c';
+	dh.checksum = ('d' << 8) | 'e';
 	EXPECT_EQ(dh.toString(), s);
 }
 
 TEST(DaneHeader, FromString) {
 	std::string s;
-	s += static_cast<unsigned char>(218);
-	s += static_cast<unsigned char>(190);
+	s += static_cast<char>(218);
+	s += static_cast<char>(190);
 	s += '\0';
 	s += "abcde";
 	DaneHeader dh;
@@ -28,4 +28,35 @@ TEST(DaneHeader, FromString) {
 	EXPECT_EQ(dh.options & 0xff, 'c');
 	EXPECT_EQ(dh.checksum >> 8, 'd');
 	EXPECT_EQ(dh.checksum & 0xff, 'e');
+}
+
+TEST(DaneHeader, FromBadMagicString) {
+	std::string s;
+	s += static_cast<char>(217);
+	s += static_cast<char>(190);
+	s += '\0';
+	s += "abcde";
+	DaneHeader dh;
+	ASSERT_FALSE(dh.fromString(s));
+}
+
+TEST(DaneHeader, FromBadVersionString) {
+	std::string s;
+	s += static_cast<char>(218);
+	s += static_cast<char>(190);
+	s += static_cast<char>(1);
+	s += "abcde";
+	DaneHeader dh;
+	ASSERT_FALSE(dh.fromString(s));
+}
+
+TEST(DaneHeader, FromStringToString) {
+	std::string s;
+	s += static_cast<char>(218);
+	s += static_cast<char>(190);
+	s += '\0';
+	s += "abcde";
+	DaneHeader dh;
+	ASSERT_TRUE(dh.fromString(s));
+	EXPECT_EQ(dh.toString(), s);
 }
